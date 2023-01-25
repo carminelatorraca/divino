87794041e1c6418d023a2eb529a84c3acb377ebb
Carmine La Torraca <carminelatorracas@gmail.com>
2023-01-25 15:53:18 +0100

    update entity bean

---
+++ implementation/divino/src/main/java/account/AccountEntity.java
@@ -0,0 +1,6 @@
+package account;
+
+public class AccountEntity {
+    private String email;
+    private String password;
+}
---
+++ implementation/divino/src/main/java/order/OrderDetail.java
@@ -0,0 +1,12 @@
+package order;
+
+import catalog.ProductEntity;
+
+public class OrderDetail {
+    private OrderEntity order;
+    private ProductEntity product;
+    private double productPrice;
+    private int productQuantity;
+    private String productDescription;
+
+}
