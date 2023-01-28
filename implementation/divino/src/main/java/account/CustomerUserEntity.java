aca95943cbf1f07270bc09b648d47723bf3cf4b8
Gaetanou <ga3tano.cataldo@gmail.com>
2023-01-29 00:11:18 +0100

    Added some Controller classes

--- implementation/divino/src/main/java/account/CustomerUserEntity.java
+++ implementation/divino/src/main/java/account/CustomerUserEntity.java
@@ -4,46 +4,27 @@ import payment.PaymentEntity;
 
 import java.util.List;
 
-public class CustomerUserEntity extends AccountEntity {
+public class CustomerUserEntity extends UserEntity {
 
-    private String firstName;
-    private String lastName;
-    private String fiscalCode;
     private String shippingAddress;
     private List<PaymentEntity> customerPayments;
 
-    public CustomerUserEntity(String email, String password, String firstName, String lastName) {
-        super(email, password, Role.CUSTOMERUSER, firstName, lastName);
-        this.fiscalCode = null;
+    public CustomerUserEntity (){
+        super("", "", Role.CUSTOMERUSER , "", "", "");
         this.shippingAddress = null;
+        this.customerPayments = null;
     }
 
-    public CustomerUserEntity() {
-        super();
+    public CustomerUserEntity(String email, String password, String firstName, String lastName, String fiscalCode, String shippingAddress, List<PaymentEntity> customerPayments) {
+        super(email, password, Role.CUSTOMERUSER, firstName, lastName, fiscalCode);
+        this.shippingAddress = shippingAddress;
+        this.customerPayments = customerPayments;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFiscalCode() {
-        return fiscalCode;
-    }
-
-    public void setFiscalCode(String fiscalCode) {
-        this.fiscalCode = fiscalCode;
+    public CustomerUserEntity(UserEntity user) {
+        super(user.getEmail(), user.getPassword(), Role.CUSTOMERUSER, user.getFirstName(), user.getLastName(), user.getFiscalCode());
+        this.shippingAddress = null;
+        this.customerPayments = null;
     }
 
     public String getShippingAddress() {
---
+++ implementation/divino/src/main/java/account/LoginServlet.java
@@ -0,0 +1,53 @@
+package account;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/login")
+
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public LoginServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+
+    }
+
+    //Check delle credenziali inserite per il login e aggiunta oggetto utente alla sessione in caso di credeziali corrette
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
+        AccountDAO dbAccount = new AccountDAO();
+        AccountEntity account = new AccountEntity();
+
+        if(request.getParameter("mode").equalsIgnoreCase("login")){
+           String email = request.getParameter("email");
+           String password = request.getParameter("password");
+
+            try {
+                account = dbAccount.retrieveAccount(email,password);
+                UserEntity user = dbAccount.retrieveUser(account);
+                if(user != null) {
+                    if (user.getRole() == AccountEntity.Role.CUSTOMERUSER) {
+                        CustomerUserEntity customer = new CustomerUserEntity(user);
+                        request.getSession().setAttribute("user", customer);
+                    } else
+                        request.getSession().setAttribute("user", user);
+                    request.getSession().setAttribute("logged", (Boolean) true);
+                    response.sendRedirect("#");
+                }else{
+                    request.getSession().setAttribute("logged", (Boolean) false);
+                    request.getSession().setAttribute("error", "Username e/o password invalidi.");
+                    response.sendRedirect("./login.jsp");
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+}
