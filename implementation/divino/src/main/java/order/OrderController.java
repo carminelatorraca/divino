1760325705715d1828832c97a0e4aaebb537e766
Carmine La Torraca <carminelatorracas@gmail.com>
2023-02-01 16:02:41 +0100

    fix

--- implementation/divino/src/main/java/order/OrderController.java
+++ implementation/divino/src/main/java/order/OrderController.java
@@ -1,9 +1,7 @@
 package order;
 
-import account.AccountDAO;
 import account.CustomerUserEntity;
 import cart.CartEntity;
-import catalog.ProductEntity;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -11,7 +9,7 @@ import payment.PaymentEntity;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Collection;
+import java.util.ArrayList;
 
 @WebServlet(name = "OrderController", value = "/buy")
 public class OrderController extends HttpServlet {
@@ -25,41 +23,69 @@ public class OrderController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        if (request.getParameter("mode").equalsIgnoreCase("showOrders")) {
+            try {
+                ArrayList<OrderEntity> orders = orderDAO.retrieveAllOrders();
+                request.getSession().setAttribute("orders", orders);
+                response.sendRedirect("./admin/order-view.jsp");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        OrderFacade orderPlacement = new OrderFacade(orderDAO);
 
-        CustomerUserEntity customer = (CustomerUserEntity) request.getSession().getAttribute("user");
-        CartEntity shoppingCart = (CartEntity) request.getSession().getAttribute("shoppingCart");
-
-        //gestire eccezione se cliente o carrello sono null
-        //if (customer == null || shoppingCart == null) {}
-
-        //logica ordine
-        try {
-            OrderEntity order = orderPlacement.placeOrder(customer);
-            order.setOrderShippingAddress((String) request.getSession().getAttribute("shippingAddress"));
-            order.setOrderTotalAmount(shoppingCart.getTotalAmount());
-            orderDAO.updateOrder(order);
-            orderPlacement.joinProducts(shoppingCart, order);
-            request.getSession().removeAttribute("shoppingCart");
-
-            request.setAttribute("order", order);
-            RequestDispatcher paymentRequest = request.getRequestDispatcher("/payment");
-            paymentRequest.include(request, response);
-
-            //verifica se il pagamento è andato a buon fine
-            PaymentEntity payment = (PaymentEntity) request.getAttribute("payment");
-            if (payment != null && payment.getPaymentStatus().equals("paid")) {
-                orderPlacement.placePayment(payment);
-                orderPlacement.placeOrderItems(order);
-                response.sendRedirect("./order-confirm.jsp");
+        System.out.println(request.getParameter("mode"));
+        System.out.println(request.getParameter("orderID"));
+        if (request.getParameter("mode").equalsIgnoreCase("updateStatus")) {
+            Integer orderID = Integer.valueOf(request.getParameter("orderID"));
+            String newStatus = request.getParameter("p_status");
+            try {
+                OrderEntity order = orderDAO.retrieveOrder(orderID);
+                System.out.println(order.getOrderTotalAmount());
+                System.out.println(order.getOrderStatus());
+                order.setOrderStatus(newStatus);
+                System.out.println(order.getOrderStatus());
+                orderDAO.updateOrder(order);
+                request.getSession().setAttribute("orders", orderDAO.retrieveAllOrders());
+                response.sendRedirect("./admin/order-view.jsp");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-        } catch (SQLException exception) {
-            exception.printStackTrace();
+        } else {
+            OrderFacade orderPlacement = new OrderFacade(orderDAO);
+
+            CustomerUserEntity customer = (CustomerUserEntity) request.getSession().getAttribute("user");
+            CartEntity shoppingCart = (CartEntity) request.getSession().getAttribute("shoppingCart");
+
+            //gestire eccezione se cliente o carrello sono null
+            //if (customer == null || shoppingCart == null) {}
+
+            //logica ordine
+            try {
+                OrderEntity order = orderPlacement.placeOrder(customer);
+                order.setOrderShippingAddress((String) request.getSession().getAttribute("shippingAddress"));
+                order.setOrderTotalAmount(shoppingCart.getTotalAmount());
+                orderDAO.updateOrder(order);
+                orderPlacement.joinProducts(shoppingCart, order);
+                request.getSession().removeAttribute("shoppingCart");
+
+                request.setAttribute("order", order);
+                RequestDispatcher paymentRequest = request.getRequestDispatcher("/payment");
+                paymentRequest.include(request, response);
+
+                //verifica se il pagamento è andato a buon fine
+                PaymentEntity payment = (PaymentEntity) request.getAttribute("payment");
+                if (payment != null && payment.getPaymentStatus().equals("paid")) {
+                    orderPlacement.placePayment(payment);
+                    orderPlacement.placeOrderItems(order);
+                    response.sendRedirect("./order-confirm.jsp");
+                }
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
         }
     }
 }
